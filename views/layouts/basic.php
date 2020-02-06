0788be2d30a0899604f71bd2042a3dee69b4d608
Igor Peshkov <igor.peshkov@cogniteq.com>
2020-02-06 18:20:37 +0300

    Add block in view and print them in layout

--- views/layouts/basic.php
+++ views/layouts/basic.php
@@ -1,6 +1,7 @@
 <?php
 
 /* @var $this \yii\web\View */
+
 /* @var $content string */
 
 use app\widgets\Alert;
@@ -42,7 +43,7 @@ AppAsset::register($this);
             ['label' => 'About', 'url' => ['/site/about']],
             ['label' => 'Contact', 'url' => ['/site/contact']],
             Yii::$app->user->isGuest ? (
-                ['label' => 'Login', 'url' => ['/site/login']]
+            ['label' => 'Login', 'url' => ['/site/login']]
             ) : (
                 '<li>'
                 . Html::beginForm(['/site/logout'], 'post')
@@ -60,11 +61,18 @@ AppAsset::register($this);
 
 
     <div class="container">
+
         <?= Breadcrumbs::widget([
             'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
         ]) ?>
         <?= Alert::widget() ?>
         <h1><?= 'Basic Layout'; ?></h1>
+
+        <!-- Check if block exist and print them. Check block creating in view file.  -->
+        <?php if (isset($this->blocks['alert'])): ?>
+            <?= $this->blocks['alert']; ?>
+        <?php endif; ?>
+
         <?= $content ?>
     </div>
 </div>
--- views/test/index.php
+++ views/test/index.php
@@ -7,3 +7,11 @@
 <div>
 	<h1>Hello</h1>
 </div>
+
+<!-- This is test block, work with theme in layout -->
+<?php $this->beginBlock('alert'); ?>
+    <div class="alert alert-warning">
+        <h4><strong>Warning!</strong></h4>
+        <p>This is very beautiful site :)</p>
+    </div>
+<?php $this->endBlock();?>
